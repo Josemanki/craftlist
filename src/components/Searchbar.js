5f070af3fe938d6e1cff78771199e2f21a0a51d4
joanmibenegas <joanmiblack@gmail.com>
2022-07-22 19:40:04 +0200

    Multilang implementation.

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,13 +1,17 @@
 import axios from "axios";
 import React, { useState } from "react";
 import SearchResults from "./SearchResults";
+import {lang} from "./Header";
 
 // Request URL https://enc.dofusdu.de/dofus/en/equipment?page%5Bnumber%5D=1&page%5Bsize%5D=20&search%5Bname%5D=harebourg
+
 
 function Searchbar({ setItemList, itemList, currentPage }) {
   const [searchQuery, setSearchQuery] = useState("");
   const [searchResults, setSearchResults] = useState([]);
   const [isFocused, setIsFocused] = useState(false);
+
+  console.log(lang)
 
   const handleFocus = () => {
     setIsFocused(true);
@@ -24,8 +28,8 @@ function Searchbar({ setItemList, itemList, currentPage }) {
     if (e.target.value.length >= 3) {
       const requestUrl =
         currentPage === "equipment"
-          ? `https://enc.dofusdu.de/dofus/en/equipment?page%5Bnumber%5D=1&page%5Bsize%5D=96&search%5Bname%5D=`
-          : `https://enc.dofusdu.de/dofus/en/resources?page%5Bnumber%5D=1&page%5Bsize%5D=96&search%5Bname%5D=`;
+          ? `https://enc.dofusdu.de/dofus/${lang}/equipment?page%5Bnumber%5D=1&page%5Bsize%5D=96&search%5Bname%5D=`
+          : `https://enc.dofusdu.de/dofus/${lang}/resources?page%5Bnumber%5D=1&page%5Bsize%5D=96&search%5Bname%5D=`;
       const res = await axios.get(`${requestUrl}${e.target.value}`);
       const filteredRes = res.data.items.filter((item) =>
         item.name.toLowerCase().includes(e.target.value.toLowerCase())
